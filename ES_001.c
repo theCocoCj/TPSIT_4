93b7e2e6515ff1b7abf1f3747453baf960c3c9b5
theCocoCj <113350295+theCocoCj@users.noreply.github.com>
2022-10-05 12:03:47 +0200

    Add files via upload

--- ES_001.c
+++ ES_001.c
@@ -4,8 +4,9 @@
 
 #define LUNG 20
 #define BUFFER_SIZE 1024
+#define NRIGHE 16600
 
-typedef char stringa[LUNG];
+typedef char* stringa;
 
 typedef struct dati {
     int rank;
@@ -16,28 +17,56 @@ typedef struct dati {
     stringa publisher;
     float NA_Sales;
     float EU_Sales;
+    float JP_Sales;
     float Other_Sales;
     float Global_Sales;
 } Dati;
 
 int main() {
-
     FILE *fp;
 
+    Dati Games[NRIGHE];
+    Dati* puntatore = Games;
     char riga[BUFFER_SIZE];
 
-    fp = fopen("vgsales.csv","r");
+    int k=0;
 
-    if(fp==NULL){
+    fp = fopen("vgsales.csv","r");
+    fgets(riga,BUFFER_SIZE, fp);
+
+    if(fp==NULL) {
 
         printf("Errore nell'apertura del file");
         return 0;
-    }else{
-        printf("Sono dentro");
-        while(!feof(fp)){
-            fgets(riga,BUFFER_SIZE, fp);
-            printf("%s", riga);
-            scanf(riga,"%s", )
+
+    } else {
+
+        printf("Sono dentro\n");
+        while(fgets(riga,BUFFER_SIZE, fp)) {
+
+            (puntatore+k)->rank=atoi(strtok(riga,","));
+            printf("%d ", (puntatore+k)->rank);
+            (puntatore+k)->name=strtok(NULL,",");
+            printf("% s ", (puntatore+k)->name);
+            (puntatore+k)->platform=strtok(NULL,",");
+            printf("% s ", (puntatore+k)->platform);
+            (puntatore+k)->year=atoi(strtok(NULL,","));
+            printf("% d ", (puntatore+k)->year);
+            (puntatore+k)->genre=strtok(NULL,",");
+            printf("% s ", (puntatore+k)->genre);
+            (puntatore+k)->publisher=strtok(NULL,",");
+            printf("% s ", (puntatore+k)->publisher);
+            (puntatore+k)->NA_Sales=atof(strtok(NULL,","));
+            printf("% .2f ", (puntatore+k)->NA_Sales);
+            (puntatore+k)->EU_Sales=atof(strtok(NULL,","));
+            printf("% .2f ", (puntatore+k)->EU_Sales);
+            (puntatore+k)->JP_Sales=atof(strtok(NULL,","));
+            printf("% .2f ", (puntatore+k)->JP_Sales);
+            (puntatore+k)->Other_Sales=atof(strtok(NULL,","));
+            printf("% .2f ", (puntatore+k)->Other_Sales);
+            (puntatore+k)->Global_Sales=atof(strtok(NULL,","));
+            printf("% .2f\n", (puntatore+k)->Global_Sales);
+            k++;
         }
     }
 
